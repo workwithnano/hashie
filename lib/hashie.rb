bcc4461a0425c370ed477b9570095d445bba9ae3
Nano <nano.anderson@gmail.com>
2012-09-20 19:54:58 -0400

    Add rubymotion code to hashie initializer file
    Signed-off-by: Nano <nano.anderson@gmail.com>

--- lib/hashie.rb
+++ lib/hashie.rb
@@ -21,3 +21,13 @@ module Hashie
     autoload :SymbolizeKeys,     'hashie/extensions/key_conversion'
   end
 end
+
+unless defined?(Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile."
+end
+
+Motion::Project::App.setup do |app|
+  Dir.glob(File.join(File.dirname(__FILE__), 'hashie/*.rb')).each do |file|
+    app.files.unshift(file)
+  end
+end
